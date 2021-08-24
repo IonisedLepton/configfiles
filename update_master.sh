d1bbd7d27d7f4a61f25d3acc0d8d2ac426de5e96
Varad <varad.sharma2718@gmail.com>
2021-08-24 17:11:22 -0400

    update_master.sh: Update from system XPS

--- update_master.sh
+++ update_master.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+
+set -e
+
 CURRENT_BRANCH_NAME=$(basename $(git symbolic-ref HEAD))
 FILE=$1
+
+if [ -z $FILE ]; then
+	echo "exit: filename not supplied as an argument"
+	exit 1
+else
+	if [ ! -e $FILE ]; then
+		echo "exit: $FILE does not exist"
+		exit 1
+	else
+		echo "Updating $FILE on master"
+	fi
+fi
 
 git checkout master
 git pull
